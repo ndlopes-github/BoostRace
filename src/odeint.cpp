760b715f46556a3a7c13c46b0cef430dd576802f
Nuno Lopes <ndlopes@gmail.com>
2021-03-19 11:44:27 +0000

    Starting c++

---
+++ src/odeint.cpp
@@ -0,0 +1,110 @@
+/*
+ Copyright 2010-2012 Karsten Ahnert
+ Copyright 2011-2013 Mario Mulansky
+ Copyright 2013 Pascal Germroth
+
+ Distributed under the Boost Software License, Version 1.0.
+ (See accompanying file LICENSE_1_0.txt or
+ copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <boost/numeric/odeint.hpp>
+#include "typedefs.h"
+
+
+//[ rhs_function
+
+const double gam = 0.15;
+
+
+//[ rhs_class
+/* The rhs of x' = f(x) defined as a class */
+class harm_osc {
+
+    double m_gam;
+
+public:
+    harm_osc( double gam ) : m_gam(gam) { }
+
+    void operator() ( const state_type &x , state_type &dxdt , const double /* t */ )
+    {
+        dxdt[0] = x[1];
+        dxdt[1] = -x[0] - m_gam*x[1];
+    }
+};
+//]
+
+
+//[ integrate_observer
+// container with the solutions at specific times
+struct push_back_state_and_time
+{
+    std::vector< state_type >& m_states;
+    std::vector< double >& m_times;
+
+    push_back_state_and_time( std::vector< state_type > &states , std::vector< double > &times )
+    : m_states( states ) , m_times( times ) { }
+
+    void operator()( const state_type &x , double t )
+    {
+        m_states.push_back( x );
+        m_times.push_back( t );
+    }
+};
+//]
+
+struct write_state
+{
+    void operator()( const state_type &x ) const
+    {
+        std::cout << x[0] << "\t" << x[1] << "\n";
+    }
+};
+
+
+int main(int /* argc */ , char** /* argv */ )
+{
+    using namespace std;
+    using namespace boost::numeric::odeint;
+
+
+    //[ state_initialization
+    state_type x(2);
+    x[0] = 1.0; // start at x=1.0, p=0.0
+    x[1] = 0.0;
+    //]
+
+
+    // //[ integration
+    // size_t steps = integrate( harmonic_oscillator ,
+    //         x , 0.0 , 10.0 , 0.1 );
+    //]
+
+
+
+    //[ integration_class
+    harm_osc ho(0.15);
+    //]
+
+
+    //[ integrate_observ
+    vector<state_type> x_vec;
+    vector<double> times;
+
+
+    //[ define_const_stepper
+    runge_kutta4< state_type > stepper;
+    size_t steps = integrate_const( stepper , ho , x , 0.0 , 10.0 , 0.01,
+                             push_back_state_and_time( x_vec , times ) );
+    //]
+
+    /* output */
+    for( size_t i=0; i<=steps; i++ )
+    {
+        cout << times[i] << '\t' << x_vec[i][0] << '\t' << x_vec[i][1] << '\n';
+    }
+
+}
---
+++ src/typedefs.h
@@ -0,0 +1,4 @@
+#include <vector>
+
+/* The type of container used to hold the state vector */
+typedef std::vector< double > state_type;
