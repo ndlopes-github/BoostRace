235e7e06a9aa7323b92c6c280e7a0cb7b915b9df
Nuno Lopes <ndlopes@gmail.com>
2021-04-27 15:43:14 +0100

    More updates

--- python/settings.py
+++ python/settings.py
@@ -4,16 +4,17 @@ from autoclass import autorepr
 
 @autorepr
 class parameters():
-    def __init__(self,timestep=0.4,
+    def __init__(self,
+                 timestep=0.4,
                  observertimestep=1.0,
                  observernsteps=7200,
                  waves=np.array([[2800,0.0,3.34],[2800,210.0,2.92],[6300,450.0,2.5]]),
                  linearfrontview=4,
                  minratio= 15./40.,
                  maxratio= 25./40.,
-                 minrho=0.4,
+                minrho=0.4,
                  maxrho=0.8,
-                 stepper=2
+                stepper=2
     ):
         self.timestep=timestep # Initial time step for solver 0.4
         self.observertimestep=observertimestep #observer time step in seconds
