b6a74788716063a7e5b5d95c7eb7802affc2561b
Nuno Lopes <ndlopes@gmail.com>
2022-06-27 23:20:32 +0100

    Starting runner  Structs

--- julia/Race.jl
+++ julia/Race.jl
@@ -7,14 +7,19 @@ import .Settings
 include("TimesGenerator.jl")
 import .TimesGenerator
 
+include("Frunnerclass.jl")
+import .Frunnerclass
 
 function model()
-    println(Settings.par)
-    println(TimesGenerator.AvgTimes,
-            TimesGenerator.RandDist,
-            TimesGenerator.NinWaves,
-            TimesGenerator.InitPositions,TimesGenerator.WaveDelays,
-            TimesGenerator.WaveInitSpeeds)
+    # println(Settings.par.trackname)
+    # println(TimesGenerator.AvgTimes,
+    #         TimesGenerator.RandDist,
+    #         TimesGenerator.NinWaves,
+    #         TimesGenerator.InitPositions,
+    #         TimesGenerator.WaveDelays,
+    #         TimesGenerator.WaveInitSpeeds)
+
+
 end
 
 
