0e4f9b97839c3b9edd25a6728f1d7e5d21fbf434
Nuno Lopes <ndlopes@gmail.com>
2022-06-27 19:28:45 +0100

    Track done and simplified

--- julia/Settings.jl
+++ julia/Settings.jl
@@ -1,7 +1,100 @@
 module Settings
 export par
+using Interpolations
 
-#
+# TODO: ORGANIZATION
+
+
+gap=180
+waves=[ 2333 500 500 0.0 3.34; 500 2333 500 (214 +1*gap) 2.92;  500 500 2334 (440.0+2*gap) 2.5]
+trackname="silvestre"
+#println(waves)
+nw=size(waves)[1]
+nr=sum(waves[:, 1:nw])
+posweights=[0.2 0; 2.0 30;  1.5 60;  1.25 120;  1.0 100000]
+
+#=
+    trackname="track1"
+    trackdata=[-500.0 0.0 10.0 ;
+          -400.0  0.0 10.0;
+          -300.0 0.0 10.0;
+          -200.0 0.0 10.0;
+          -100.0 0.0 10.0;
+          0.0 0.0 10.0;
+          100.0 0.0 10.0;
+          1000.0 100.0 10.0;
+          2000.0 -50.0 10.0;
+          3000.0 -30.0 10.0;
+          4000.0 120.0 10.0;
+          5000.0 -27.0 10.0;
+          6000.0 59.00 10.0;
+          7000.0 -100.0 10.0;
+          8000.0 -120.0 10.0;
+          9000.0 190.0 10.0;
+          10000.0 10.0 10.0;
+          10100.0 10.0 10.0;
+          10200.0 10.0 10.0]
+=#
+
+#=
+    trackname="track2"
+    trackdata=[-500.0 0.0 10.0;
+          -400.0 0.0 10.0;
+          -300.0 0.0 10.0;
+          -200.0 0.0 10.0;
+          -100.0 0.0 10.0;
+          0.0 0.0 10.0;
+          100.0 0.0 10.0;
+          1000.0 10.0 10.0;
+          2000.0 -2.0 5.0;
+          2200.0 -2.0 3.0;
+          2300.0 -1.0 3.0;
+          2400.0 -1.0 3.0;
+          2500.0 -1.0 6.0;
+          3000.0 -15.0 10.0;
+          4000.0 20.0 2.0;
+          5000.0 -7.0 10.0;
+          6000.0 9.0 10.0;
+          7000.0 -10.0 10.0;
+          8000.0 -1.0 7.0;
+          9000.0 1.0 10.0;
+          10000.0 0.0 10.0;
+          10100.0 0.0 10.0;
+          10200.0 0.0 10.0]
+=#
+
+trackname="silvestre"
+data_aux=[-500.0 10.0 16.0;
+          220.0 (-0.02*220.0) 16.0;
+          260.0 (-0.02*260.0) 11.0;
+          480.0 (-0.02*480.0) 11.0;
+          520.0 (-0.02*520.0) 6.0;
+          1000.0 -20.0 6.0;
+          1040.0 -20.0 8.0;
+          1500.0 -20.0 8.0;
+          2000.0 -20.0 8.0;
+          6000.0 -20.0 8.0;
+          6500.0 -20.0 8.0;
+          7460.0 13.6 8.0;
+          7500.0 15.0 16.0;
+          8500.0 50.0 16.0;
+          10000.0 0.0 16.0;
+          10500.0 (-1050.0/3+1000.0/3.0) 16.0]
+N=501
+#auxwidth=zeros(N)
+#auxlevel=zeros(N)
+x=range(start=-500.0,stop=10500,length=N)
+aux_diff_lspline=LinearInterpolation(data_aux[:,1],data_aux[:,2])
+aux_width_lspline=LinearInterpolation(data_aux[:,1],data_aux[:,3])
+auxlevel=aux_diff_lspline.(x)
+auxwidth=aux_width_lspline.(x)
+trackdata=zeros(N,3)
+for i in range(1,N)
+    trackdata[i,1]=x[i]
+    trackdata[i,2]=auxlevel[i]
+    trackdata[i,3]=auxwidth[i]
+end
+
 
 struct parameters
     timestep
@@ -18,17 +111,11 @@ struct parameters
     ldist
     numberofwaves
     nrunners
-    track
+    trackname
+    trackdata
 end
 
-gap=180
-waves=[ 2333 500 500 0.0 3.34; 500 2333 500 (214 +1*gap) 2.92;  500 500 2334 (440.0+2*gap) 2.5]
-track="track3"
-#println(waves)
-nw=size(waves)[1]
-nr=sum(waves[:, 1:nw])
-posweights=[0.2 0; 2.0 30;  1.5 60;  1.25 120;  1.0 100000]
+par=parameters(0.4,1.0,7080, 7080, waves,4,(15.0/40.0), (25.0/40.0),0.4,0.8,posweights,0.5, nw, nr, trackname,trackdata)
 
-par=parameters(0.4,1.0,7080, 7080, waves,4,(15.0/40.0), (25.0/40.0),0.4,0.8,posweights,0.5, nw, nr, track)
 
 end
--- julia/Track.jl
+++ julia/Track.jl
@@ -1,104 +1,13 @@
 module Track
 export track
 using CubicSplines
-using Interpolations
 using Plots
 plotlyjs()
 #pyplot()
 include("Settings.jl")
 import .Settings
-track_choice=Settings.track
-
-
-if track_choice=="track1"
-    ## Track1 ##
-    name="imaginary1"
-    data=[-500.0 0.0 10.0 ;
-          -400.0  0.0 10.0;
-          -300.0 0.0 10.0;
-          -200.0 0.0 10.0;
-          -100.0 0.0 10.0;
-          0.0 0.0 10.0;
-          100.0 0.0 10.0;
-          1000.0 100.0 10.0;
-          2000.0 -50.0 10.0;
-          3000.0 -30.0 10.0;
-          4000.0 120.0 10.0;
-          5000.0 -27.0 10.0;
-          6000.0 59.00 10.0;
-          7000.0 -100.0 10.0;
-          8000.0 -120.0 10.0;
-          9000.0 190.0 10.0;
-          10000.0 10.0 10.0;
-          10100.0 10.0 10.0;
-          10200.0 10.0 10.0]
-end
-
-if track_choice=="track2"
-    name="imaginary2"
-    data=[-500.0 0.0 10.0;
-          -400.0 0.0 10.0;
-          -300.0 0.0 10.0;
-          -200.0 0.0 10.0;
-          -100.0 0.0 10.0;
-          0.0 0.0 10.0;
-          100.0 0.0 10.0;
-          1000.0 10.0 10.0;
-          2000.0 -2.0 5.0;
-          2200.0 -2.0 3.0;
-          2300.0 -1.0 3.0;
-          2400.0 -1.0 3.0;
-          2500.0 -1.0 6.0;
-          3000.0 -15.0 10.0;
-          4000.0 20.0 2.0;
-          5000.0 -7.0 10.0;
-          6000.0 9.0 10.0;
-          7000.0 -10.0 10.0;
-          8000.0 -1.0 7.0;
-          9000.0 1.0 10.0;
-          10000.0 0.0 10.0;
-          10100.0 0.0 10.0;
-          10200.0 0.0 10.0]
-end
-
-
-if track_choice=="track3"
-    name="silvestre"
-    data_aux=[-500.0 10.0 16.0;
-              220.0 (-0.02*220.0) 16.0;
-              260.0 (-0.02*260.0) 11.0;
-              480.0 (-0.02*480.0) 11.0;
-              520.0 (-0.02*520.0) 6.0;
-              1000.0 -20.0 6.0;
-              1040.0 -20.0 8.0;
-              1500.0 -20.0 8.0;
-              2000.0 -20.0 8.0;
-              6000.0 -20.0 8.0;
-              6500.0 -20.0 8.0;
-              7460.0 13.6 8.0;
-              7500.0 15.0 16.0;
-              8500.0 50.0 16.0;
-              10000.0 0.0 16.0;
-              10500.0 (-1050.0/3+1000.0/3.0) 16.0]
-    N=501
-    auxwidth=zeros(N)
-    auxelev=zeros(N)
-    x=range(start=-500.0,stop=10500,length=N)
-
-    aux_diff_lspline=LinearInterpolation(data_aux[:,1],data_aux[:,2])
-    aux_width_lspline=LinearInterpolation(data_aux[:,1],data_aux[:,3])
-    auxlevel=aux_diff_lspline.(x)
-    auxwidth=aux_width_lspline.(x)
-    data=zeros((size(x),3))
-    for i in range(1,size(x))
-        data[i,1]=x[i]
-        data[i,2]=auxlev[i]
-        data[i,3]=auxwidth[i]
-    end
-
-end
-
-
+trackname=Settings.trackname
+data=Settings.trackdata
 
 struct track_struct
     name
@@ -109,29 +18,21 @@ struct track_struct
     cspline2
 end
 
-
 x_data=data[:,1]
 diff_data=data[:,2]
 width_data=data[:,3]
 cspline=CubicSpline(x_data,diff_data)
 cspline2=CubicSpline(x_data,width_data)
-track=track_struct(name,x_data,diff_data,width_data,cspline,cspline2)
+track=track_struct(trackname,x_data,diff_data,width_data,cspline,cspline2)
 
 
 
 function plot_diff_track(track)
     xs = range(start=minimum(track.x_data), stop=maximum(track.x_data), length=1000)
-    #fig, ax = plt.subplots(figsize=(6.5, 4))
-    #ax.plot(self.x_data,self.diff_data, 'o', label='data')
     ys=track.cspline(xs)
     plot(xs, ys,title="Elevation (m)",reuse=false)
     gui()
-    #ax.plot(xs, self.cspline(xs,1), label="S'",color='k')
-    #ax.set_xlim(xs.min()-10, xs.max()+10)
-    #ax.legend(loc='best', ncol=1)
-    #ax.xaxis.set_major_locator(MultipleLocator(1000))
-    #ax.grid()
-    #plt.show()
+
 end
 
 function plot_width_track(track)
@@ -139,33 +40,20 @@ function plot_width_track(track)
     #fig, ax = plt.subplots(figsize=(6.5, 4))
     #ax.plot(self.x_data,self.diff_data, 'o', label='data')
     ys=track.cspline2(xs)
-    plot(xs, ys,reuse=false)
+    plot(xs, ys,reuse=false, title="Width (m)")
     gui()
-    #ax.plot(xs, self.cspline(xs,1), label="S'",color='k')
-    #ax.set_xlim(xs.min()-10, xs.max()+10)
-    #ax.legend(loc='best', ncol=1)
-    #ax.xaxis.set_major_locator(MultipleLocator(1000))
-    #ax.grid()
-    #plt.show()
 end
 
-function plot_slope_track( track)
+function plot_slope_track(track)
     xs = range(start=minimum(track.x_data), stop=maximum(track.x_data), length=1000)
-    #fig, ax = plt.subplots(figsize=(6.5, 4))
-    #ax.plot(np.linspace(0, self.length, len(self.diff_data)),self.diff_data, 'o', label='data')
-    #ax.plot(xs, self.cspline(xs), label="S")
     spline=track.cspline
     slope=zeros(0)
     for x in xs
         append!(slope, 100*gradient(spline,x,1))
     end
-    plot(xs, slope,reuse=false)
+    plot(xs, slope,reuse=false,title="Slope (%)")
     gui()
-    # ax.set_xlim(xs.min()-10, xs.max()+10)
-    #ax.legend(loc='best', ncol=1)
-    #ax.xaxis.set_major_locator(MultipleLocator(1000))
-    #ax.grid()
-    #plt.show()
+
 end
 
 plot_diff_track(track)
